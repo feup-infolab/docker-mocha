f6b77cd07430266fbb29759a8f8d60bc6b5e4655
Nuno Neto <nuno_mgl@hotmail.com>
2019-03-25 16:00:51 +0000

    added init and setup files

---
+++ setup/init.js
@@ -0,0 +1,57 @@
+const assert = require('assert');
+const request = require('request');
+const expressServer = require('../src/index');
+const port = 3000;
+
+beforeEach(() =>
+{
+    expressServer.start(null, port);
+});
+
+afterEach(() =>
+{
+    expressServer.stop();
+});
+
+
+describe('Testing server requests', function()
+{
+    describe('Testing hello world', function ()
+    {
+        this.timeout(5000);
+
+        it('should say "Hello, world!"', (done) =>
+        {
+            request('http://localhost:' + port, {json:true}, (err, res, body) =>
+            {
+                assert.equal(200, res.statusCode);
+                assert.equal(body, "Hello World!");
+                done();
+            })
+        })
+    });
+
+    describe('Testing init', function ()
+    {
+        this.timeout(5000);
+
+        it('should give error code 0"', (done) =>
+        {
+            request('http://localhost:' + port + '/init', {json:true}, (err, res, body) =>
+            {
+                assert.equal(200, res.statusCode);
+                assert.equal(body.error, 0);
+                done();
+            })
+        })
+
+    });
+});
+
+exports.setup = (done) =>
+{
+    request('http://localhost:' + port, {json:true}, (err, res, body) =>
+    {
+        done();
+    })
+};
---
+++ setup/pound/testPound.js
@@ -0,0 +1,45 @@
+const assert = require('assert');
+const request = require('request');
+const setPound = require("../pound/setPound");
+const port = 3000;
+
+const pound = 0.86;
+const euro = 25.57;
+const value = (pound * euro).toFixed(2);
+
+
+describe('Testing get Pound', function()
+{
+    this.timeout(5000);
+
+    it('should give error code 0 and return converted value', (done) =>
+    {
+
+        request('http://localhost:' + port + '/get/pound?euro=' + euro, {json:true}, (err, res, body) =>
+        {
+            assert.equal(200, res.statusCode);
+            assert.equal(body.error, 0);
+            assert.equal(body.pounds, value);
+            done();
+        })
+    }, pound);
+});
+
+
+
+/**
+ * Not used for now
+ */
+
+/*
+ exports.setup = (done) =>
+ {
+    setPound.setup(() =>
+    {
+        request('http://localhost:' + port + '/add/pound?value=' + pound, {json:true}, (err, res, body) =>
+        {
+            done();
+        })
+    });
+};
+*/
