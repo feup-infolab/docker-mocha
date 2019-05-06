b04962e6d9a1818b227f9f9761223e870a4f8e27
Nuno Neto <nuno_mgl@hotmail.com>
2019-05-06 14:31:19 +0100

    updated DockerManager

--- src/DockerManager.js
+++ src/DockerManager.js
@@ -41,9 +41,9 @@ DockerManager.deleteAllStates = function(dockerMocha, callback)
         async.mapSeries(services,
             (service, callback) =>
             {
-                console.log("Deleting all states", `'docker images | grep ${service.image} | tr -s ' ' | cut -d ' ' -f 2 | xargs -I {} docker rmi ${service.image}:{} -f'`);
+                console.log("Deleting all states", `'docker rmi $(docker images --filter=reference=${service.image}:${service.tag}[A-Za-z0-9][A-Za-z0-9]* -q) -f'`);
 
-                childProcess.exec(`docker images | grep ${service.image} | tr -s ' ' | cut -d ' ' -f 2 | xargs -I {} docker rmi ${service.image}:{} -f`,
+                childProcess.exec(`docker rmi $(docker images --filter=reference=${service.image}:${service.tag}[A-Za-z0-9][A-Za-z0-9]* -q) -f`,
                     (err, result) =>
                     {
                         callback();
